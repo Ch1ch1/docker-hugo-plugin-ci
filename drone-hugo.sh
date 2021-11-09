b3c8dc45114606cac9a43e5eda5b5de08c2ae366
Frederic Senchet <fred.chinchin@gmail.com>
2021-11-09 04:28:59 +0100

    Update drone-hugo.sh

--- drone-hugo.sh
+++ drone-hugo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-HUGO_VERSION=${PLUGIN_HUGO_VERSION:-"0.88.1"}
+HUGO_VERSION=${PLUGIN_HUGO_VERSION:-"0.89.2"}
 HUGO_ARCH="64bit"
 
 HUGO_URL="https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-${HUGO_ARCH}.tar.gz"
